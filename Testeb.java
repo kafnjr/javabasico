e0d99b6b4c2c6254d708881aab859748b6694107
Carlos Junior <cajunior.ps@outlook.com>
2018-02-28 21:25:24 -0300

    Primeira carga de arquivos de exercícios da aula JavaBasico

---
+++ Testeb.java
@@ -0,0 +1,11 @@
+public class Testeb {
+	public static void main(String[] args){
+		int x = 0;
+		int y = 0;
+		while (x < 5) {
+			y = y + x;
+			System.out.println(x + " " + y + " ");
+			x = x + 1;
+		}
+	}
+}
