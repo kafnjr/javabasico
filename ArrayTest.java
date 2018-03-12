b967be9a8ceda389f274477284d4754cc92bbfb6
Carlos Junior <cajunior.ps@outlook.com>
2018-03-11 23:47:37 -0300

    Breve introdução sobre arrays de tipo primitivo em java e teste de impressão das variáveis, o objetivo aqui é apenas observar o comportamento e não a correta execução utilizando laços de repetição

---
+++ ArrayTest.java
@@ -0,0 +1,19 @@
+public class ArrayTest {
+	public static void main(String[] args) {
+		int[] nums = new int[7];
+		nums[0]= 34;
+		nums[1]= 4;
+		nums[2]= 6;
+		nums[3]= 7;
+		nums[4]= 22;
+		nums[5]= 15;
+		nums[6]= 11;
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+		System.out.println(nums[2]);
+		System.out.println(nums[3]);
+		System.out.println(nums[4]);
+		System.out.println(nums[5]);
+		System.out.println(nums[6]);
+	}
+}
